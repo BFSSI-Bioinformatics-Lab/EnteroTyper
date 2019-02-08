9af0ef2f0e24f1305aef85adde05ba64e8f48079
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-02-08 14:46:02 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from __init__ import __version__, __author__, __email__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EnteroTyper",
+    install_requires=['click', 'pandas', 'xlsxwriter', 'tqdm'],
+    python_requires='~=3.7',
+    description="Scripts to type and compare assemblies based off of Enterobase schemes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BFSSI-Bioinformatics-Lab/EnteroTyper",
+    package_data={'EnteroTyper': ['*']},
+    install_pacakage_data=True,
+    packages=setuptools.find_packages(),
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    entry_points={
+        'console_scripts': [
+            'enterotyper=EnteroTyper.enterotyper:cli'
+        ]
+    }
+)
