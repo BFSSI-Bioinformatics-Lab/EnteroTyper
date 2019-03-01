f6cb251a73633120dca0709f2a3bee8ed7020aae
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-03-01 14:25:50 -0500

    Initial commit

---
+++ EnteroTyper/tests/test_compare_sequence_types.py
@@ -0,0 +1,25 @@
+from EnteroTyper.bin.compare_sequences_types import *
+
+
+def test_call_sequence_comparison():
+    pass
+
+
+def test_compare_reports():
+    pass
+
+
+def test_read_allele_report():
+    pass
+
+
+def test_compare_keys():
+    pass
+
+
+def test_compare_allele_dicts():
+    pass
+
+
+def test_count_mismatches():
+    pass
